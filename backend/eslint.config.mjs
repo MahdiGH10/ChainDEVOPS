30775db187b8a12f1bda184b9b6de01c0dbc52b5
MahdiGH10 <mgharbi031@gmail.com>
2026-04-29 09:23:02 +0000

    add: add backend test and fix lint version

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,10 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
-import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.node }
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs", globals: globals.node },
+    rules: {
+      // allow unused function arguments (common in Express middleware: err, req, res, next)
+      "no-unused-vars": ["error", { "args": "none" }]
+    }
+  }
 ]);
